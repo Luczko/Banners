a0f49f105ecf62aeaa57d09530753eef982ef22c
Luczko <wlalukasz@gmail.com>
2021-04-29 11:07:06 +0200

    splitIntoComponentsPt2

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
+  @Output() clickOne = new EventEmitter<any>();
+  @Output() clickTwo = new EventEmitter<any>();
+  @Input() colors: any;
+  @Input() percentColor1: any;
+  @Input() percentColor2: any;
+  @Input() width: any;
+  @Input() height: any;
+  @Input() fontSize: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  changeColorOne(color: string): void {
+    this.clickOne.emit(color);
   }
-
+  changeColorTwo(color: string): void {
+    this.clickTwo.emit(color);
+  }
 }
